3adcdd3198a6f031059f605fd0039070f73bd080
Andres Juan <andresjuanpelaez@gmail.com>
2025-01-06 19:30:56 -0500

    Update a comment

--- 01.py
+++ 01.py
@@ -1,4 +1,4 @@
 counter = 0 #Variable to store the count of the loop.
 for _ in range(10): #Loop to run 10 times.
-    counter = counter + 1 #Increment the counter variable by 1.
+    counter = counter + 1 #Increment the counter variable by 1
     print(counter) #Print the value of the counter variable.
